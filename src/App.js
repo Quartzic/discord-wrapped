97f44a2ed8de8478b2e1668cb2f3e44a50c1404f
Nathan Goller-Deitsch <nathan@quartzic.co>
2022-02-22 12:59:42 -0500

    reformat header

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ function App() {
   return (
     <>
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8">
-        <h2 className="text-2xl font-bold leading-7 p-2 text-gray-900 sm:text-3xl sm:truncate">
-          Discord Wrapped
-        </h2>
-
+      <div className="my-8">
+          <h1 className="text-3xl font-bold">📊 Discord Wrapped</h1>
+          <p className="my-2">
+          Explore your Discord chat history to find trends, patterns, and cool facts.
+          </p>
+        </div>
         <Main />
       </div>
       <footer className="bg-white my-6">
-          <div className="mt-8 md:mt-0 md:order-1">
-            <p className="text-center text-base text-gray-400">
-              Made with ❤️ by the Infinite Monkeys for HackMIT Blueprint '22
-            </p>
-          </div>
-        </footer>
+        <div className="mt-8 md:mt-0 md:order-1">
+          <p className="text-center text-base text-gray-400">
+            Made with ❤️ by the Infinite Monkeys for HackMIT Blueprint '22
+          </p>
+        </div>
+      </footer>
     </>
   );
 }
